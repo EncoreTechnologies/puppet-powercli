388c664dc4a84486aa36ef8b77a9cd507575979c
Greg Perry <greg.perry@encore.tech>
2019-03-13 15:42:38 -0400

    CLOUD-1222 #iscsi_target.pp working version - needs tested

--- manifests/esx/iscsi_target.pp
+++ manifests/esx/iscsi_target.pp
@@ -1,37 +1,33 @@
 define powercli::esx::iscsi_target (
+  $hostname,
   $discovery,
-  $iscsi_name,
   $targets,
-  $chap_user,
-  $chap_pass,
   $port,
-  $hostname
+  Optional[String] $chap_user = undef,
+  Optional[String] $chap_pass = undef,
+  Optional[String] $iscsi_name = undef,
+  Optional[String] $san_name = undef
 ) {
   include powercli::vcenter::connection
   $_connect = $powercli::vcenter::connection::connect
-
-notify{"Name is: ${name}" : }
-notify{"hostname is: ${hostname}" : }
-notify{"port is: ${port}" : }
-
-  # Filtering out any target without a discovery type OR a discovery type different than known types
-  # if ($discovery == 'dynamic') or ($discovery == 'static') {
-  #   $targets.each | $target | {
-  #       exec { "${name}: Add ${array} iSCSI target: ${target} ${chap_str} with ${discovery} discovery":
-  #       command  => template('powercli/powercli_esx_iscsi_targets.ps1.erb'),
-  #       provider => 'powershell',
-  #       onlyif   => template('powercli/powercli_esx_iscsi_targets_onlyif.ps1.erb'),
-  #       # Tag this resource so we can reference later for rescans
-  #       tag      => "powercli::esx::iscsi_targets_${name}",
-  #     }
-  #   }
-  # }
-  # else{
-  #   fail("iSCSI Array: ${array} has discovery type of: (${discovery})."
-  #         + 'This does not match known discovery types. '
-  #         + "Known discovery types are: 'static' or 'dynamic'.")
-  # }
-
+  
+  #Filtering out any target without a discovery type OR a discovery type different than known types
+  if ($discovery == 'dynamic') or ($discovery == 'static') {
+    $targets.each | $target | {
+        exec { "${hostname} add target: ${target}":
+        command  => template('powercli/powercli_esx_iscsi_targets.ps1.erb'),
+        provider => 'powershell',
+        onlyif   => template('powercli/powercli_esx_iscsi_targets_onlyif.ps1.erb'),
+        # Tag this resource so we can reference later for rescans
+        tag      => "powercli::esx::iscsi_targets_${name}",
+      }
+    }
+  }
+  else{
+    fail("iSCSI Array: ${san_name} has discovery type of: (${discovery})."
+          + 'This does not match known discovery types. '
+          + "Known discovery types are: 'static' or 'dynamic'.")
+  }
 
   # # Calls the rescan resource but it does not run because the exec within the rescan resource is `refreshonly`
   # powercli::esx::iscsi_rescan { $name: }
