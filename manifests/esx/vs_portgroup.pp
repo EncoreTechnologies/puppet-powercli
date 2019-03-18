ee01049bbd3776724b5835037637ce2f23b7e680
Greg Perry <greg.perry@encore.tech>
2019-03-18 15:02:28 -0400

    moving to vs_portgroup

--- manifests/esx/vs_portgroup.pp
+++ manifests/esx/vs_portgroup.pp
@@ -1,18 +1,35 @@
+# @summary Resource that manages ESX Standard vSwitch portgroups
+#
+# @param hostname
+#   The hostname of the host we want to create the vswitch on
+# @param vswitch_name
+#   The name of the vswitch we want to create portgroups on
+# @param vlan
+#   The VLAN ID of the portgroup being created
+# @param portgroup
+#   The name of the newly created portgroup
+#
+# @example Basic usage
+#  powercli::esx::vs_portgroup {
+#    hostname     => 'my-vmware-host.fqdn.tld',
+#    vswitch_name => 'vswitch0',
+#    vlan         => 10
+#    portgroup    => 'vlan_10_portgroup_name'
+#  }
 define powercli::esx::vs_portgroup (
+  $hostname,
   $vswitch_name,
-  $vs_pgs,
+  $vlan,
+  $portgroup
 ) {
   include powercli::vcenter::connection
   $_connect = $powercli::vcenter::connection::connect
+  
+  $_cmd = "Get-VirtualSwitch -Standard -VMhost ${hostname} -Name ${vswitch_name} | New-VirtualPortgroup -Name ${portgroup} -VLanID ${vlan}"
 
-  $vs_pgs.each | $pg_name, $vlan | {
-    $vlan_id = $vlan['vlan']
-    $_cmd = "Get-VirtualSwitch -Standard -VMhost ${name} -Name ${vswitch_name} | New-VirtualPortgroup -Name ${pg_name} -VLanID ${vlan_id}"
-
-    exec { "Create Portgroup ${pg_name} on host ${name}":
-      command  => "${_connect}; ${_cmd} ",
-      provider => 'powershell',
-      onlyif   => template('powercli/powercli_esx_vs_portgroups_onlyif.ps1.erb'),
-    }
+  exec { "Create Portgroup ${portgroup} on host ${hostname}":
+    command  => "${_connect}; ${_cmd} ",
+    provider => 'powershell',
+    onlyif   => template('powercli/powercli_esx_vs_portgroups_onlyif.ps1.erb'),
   }
 }
