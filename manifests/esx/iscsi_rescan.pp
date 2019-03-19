5506e72e5006e136463c007680ae76c8835c3191
Greg Perry <greg.perry@encore.tech>
2019-03-19 09:26:38 -0400

    added puppet strings

--- manifests/esx/iscsi_rescan.pp
+++ manifests/esx/iscsi_rescan.pp
@@ -1,3 +1,11 @@
+# @summary Resource that manages storage HBA rescans
+#
+# @param hostname
+#   Name of the host we wish to rescan on
+# @example Basic usage
+#   powercli::esx::iscsi_rescan {
+#     hostname => 'my-vmware-host.fqdn.tld',
+#   }
 define powercli::esx::iscsi_rescan (
   $hostname,
 ) {
