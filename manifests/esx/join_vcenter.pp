cc230ab5ad9d6a2380a451ad4020e982884de52e
Greg Perry <greg.perry@encore.tech>
2019-07-17 14:52:29 -0400

    changed debug msg

--- manifests/esx/join_vcenter.pp
+++ manifests/esx/join_vcenter.pp
@@ -22,7 +22,7 @@ define powercli::esx::join_vcenter (
   $_connect = $powercli::vcenter::connection::connect
 $_cmd = "Add-VMHost -Name '${name}' -User '${host_user}' -Password '${host_password}' -Location '${host_location}' -Force"
 
-  notify{"TEST: ${name} || ${_cmd}" : }
+  notify{"TEST: ${name} || ${_connect}" : }
 
   exec { "Join host to cluster - ${name}:":
     command  => "${_connect}; ${_cmd}",
