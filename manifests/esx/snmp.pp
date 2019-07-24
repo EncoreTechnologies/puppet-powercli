b0568b9b43ee3aaa170586c6b00b220adb8e6f36
Greg Perry <greg.perry@encore.tech>
2019-07-24 12:18:10 -0400

    CLOUD-1108 # moving to handle multiple targets in powershell rather than puppet

--- manifests/esx/snmp.pp
+++ manifests/esx/snmp.pp
@@ -25,17 +25,16 @@ define powercli::esx::snmp (
   $community,
   $port,
   $targets,
-) {
-  $targets.each | $target | {
-    exec { "${name} Set SNMP target ${target}":
-      command  => template('powercli/powercli_esx_snmp.ps1.erb'),
-      provider => 'powershell',
-      onlyif   => template('powercli/powercli_esx_snmp_onlyif.ps1.erb'),
-    }
-    powercli::esx::service {"${name} - snmpd":
-      service   => 'snmpd',
-      host      => $name,
-      subscribe => Exec["${name} Set SNMP target ${target}"],
-    }
+)
+{
+  exec { "${name} Set SNMP target ${targets}":
+    command  => template('powercli/powercli_esx_snmp.ps1.erb'),
+    provider => 'powershell',
+    onlyif   => template('powercli/powercli_esx_snmp_onlyif.ps1.erb'),
+  }
+  powercli::esx::service {"${name} - snmpd":
+    service   => 'snmpd',
+    host      => $name,
+    subscribe => Exec["${name} Set SNMP targets ${targets}"],
   }
 }
