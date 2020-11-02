f6ae85433e6f1c41d485a1df0b5d99f62eef03f7
Greg Perry <greg.perry@encore.tech>
2020-11-02 14:08:07 -0500

    test catching nil returns

--- lib/puppet/provider/powercli_esx_syslog/powercli.rb
+++ lib/puppet/provider/powercli_esx_syslog/powercli.rb
@@ -34,39 +34,36 @@ Puppet::Type.type(:powercli_esx_syslog).provide(:api, parent: Puppet::Provider::
 
     syslog_servers_stdout = powercli_connect_exec(cmd)[:stdout]
 
-    # what happens if this returns null??
-    syslog_servers_hash = JSON.parse(syslog_servers_stdout)
-
-    cached_instances_set({})
-
-    syslog_servers_hash.each do |esx_host, syslog_server_info_array|
-      # Because the syslog_server_info_array has multiple elements (Host and Port)
-      # It will return as an array.
-      syslog_server_info = syslog_server_info_array[0]
-      # Example url: udp://192.168.1.10
-      uri = URI(syslog_server_info['Host'])
-      cached_instances[esx_host] = {
-        ensure: :present,
-        esx_host: esx_host,
-        syslog_server: uri.host,
-        syslog_protocol: uri.scheme,
-        syslog_port: syslog_server_info['Port'],
-      }
+    # Verify we got a non-null return from powershell
+    unless syslog_servers_stdout.nil?
+      syslog_servers_hash = JSON.parse(syslog_servers_stdout)
+      cached_instances_set({})
+      syslog_servers_hash.each do |esx_host, syslog_server_info_array|
+        # Because the syslog_server_info_array has multiple elements (Host and Port)
+        # It will return as an array.
+        syslog_server_info = syslog_server_info_array[0]
+        # Example url: udp://192.168.1.10
+        uri = URI(syslog_server_info['Host'])
+        cached_instances[esx_host] = {
+          ensure: :present,
+          esx_host: esx_host,
+          syslog_server: uri.host,
+          syslog_protocol: uri.scheme,
+          syslog_port: syslog_server_info['Port'],
+        }
+        cached_instances
+      end
     end
-
-    cached_instances
   end
 
   def read_instance
     if all_instances.key?(resource[:esx_host])
       all_instances[resource[:esx_host]]
-      test = all_instances
     else
       {
         ensure: :absent,
         esx_host: resource[:esx_host],
       }
-      Puppet.debug("read_instances test is: #{test}")
     end
   end
 
