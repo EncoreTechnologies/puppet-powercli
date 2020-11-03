4ebda04bedbdba68d85c62e1c74c83456530cb38
Greg Perry <greg.perry@encore.tech>
2020-11-03 12:40:13 -0500

    fixing data type validation

--- lib/puppet/type/powercli_esx_vs_portgroup.rb
+++ lib/puppet/type/powercli_esx_vs_portgroup.rb
@@ -5,29 +5,37 @@ Puppet::Type.newtype(:powercli_esx_vs_portgroup) do
 
   newparam(:esx_host) do
     desc 'The name of the esx host in which the standard vswitch will be managed on'
-    unless value.is_a?(String)
-      raise ArgumentError, "esx_host is expected to be a String, given: #{value.class.name}"
+    validate do |value|
+      unless value.is_a?(String)
+        raise ArgumentError, "esx_host is expected to be a String, given: #{value.class.name}"
+      end
     end
   end
 
   newproperty(:vswitch_name) do
     desc 'Name of the vswitch which will be used on the esx_host'
-    unless value.is_a?(String)
-      raise ArgumentError, "vswitch_name is expected to be a String, given: #{value.class.name}"
+    validate do |value|
+      unless value.is_a?(String)
+        raise ArgumentError, "vswitch_name is expected to be a String, given: #{value.class.name}"
+      end
     end
   end
 
   newproperty(:vlan) do
     desc 'VLAN ID to be assigned to the portgroup'
-    unless value.is_a?(Integer)
-      raise ArgumentError, "vlan is expected to be a Integer, given: #{value.class.name}"
+    validate do |value|
+      unless value.is_a?(Integer)
+        raise ArgumentError, "vlan is expected to be a Integer, given: #{value.class.name}"
+      end
     end
   end
 
   newproperty(:portgroup) do
-    desc 'Name of the portgroup which will be managed on the esx_host'
-    unless value.is_a?(String)
-      raise ArgumentError, "portgroup is expected to be a String, given: #{value.class.name}"
+    validate do |value|
+      desc 'Name of the portgroup which will be managed on the esx_host'
+      unless value.is_a?(String)
+        raise ArgumentError, "portgroup is expected to be a String, given: #{value.class.name}"
+      end
     end
   end
 
