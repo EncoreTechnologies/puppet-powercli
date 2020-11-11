ffca4dd5f91a9d5b2010cd18c7d8faee5272bae3
Greg Perry <greg.perry@encore.tech>
2020-11-11 10:59:34 -0500

    fixed comments

--- manifests/esx/dvs_add_hosts.pp
+++ manifests/esx/dvs_add_hosts.pp
@@ -3,57 +3,15 @@
 # @param esx_host
 #   Name or IP address of the ESX host you would like joined to the dvswitch.
 # @param dvswitch_name
-#   Name of the dvs.
-# @param dvswitch_nic_defaults
-#   Hash of physical NIC layout defaults. Generally your 'defaults' will be your most common hardware
-#   model.
-#   Example: Assume you have the following;
-#     - 10 ESX hosts where vmnic0 & 1 are 10GB adapters and vmnic2 & 3 are 1GB adapters
-#     - 1 ESX host where vmnic2 & 3 are 10GB and vmnic0 & 1 are 1GB (opposite of above)
-#   You would set dvswitch_nic_defaults to vmnic0 & 1 for distributed vswitch, and
-#   vmnic2 & 3 are for standard vswitch. It would look like:
-#   dvswitch_nic_defaults => { 'nics' => [ 'vmnic0', 'vmnic1'] }
-# @param overrides
-#   Hash of physical NIC overrides for ESX hosts that do not match your dvswitch_nic_defaults. In
-#   the example above, overrides would be used for the host that is unlike the rest. 
-#   Assuming your defaults looked like;
-#   dvswitch_nic_defaults => { 'nics' => [ 'vmnic0', 'vmnic1'] }
-#   your overrides for the host that do not match the others would look like;
-#   overrides => { 'nics' => [ 'vmnic2', 'vmnic3'] }
+#   Name of the dvswitch.
+# @param nics
+#   Array of NICs to be attached to the given dvswitch
 #
-# @example Basic usage - Using Default NICs on host. This will attach vmnic0 & 1 to the dvs.
+# @example Usage This will attach vmnic2 & 3 to the dvs.
 #   powercli::esx::dvs_add_hosts {'my-vmware-host.fqdn.tld':
 #     esx_host      => 'my_esx_hostname_or_ip_here',
 #     dvswitch_name => 'my_dvswitch_name_here',
-#     dvswitch_nic_defaults => {
-#     'my_dvswitch_name_here' => {
-#       'type' => distributed,
-#       'nics' => [ 'vmnic0', 'vmnic1']},
-#     'my_svswitch_name_here' => {
-#       'type' => standard,
-#       'nics' => [ 'vmnic2', 'vmnic3']}
-#     },
-#   }
-# @example Override usage - Using Override NICs on host. This will attach vmnic2 & 3 to the dvs.
-#   powercli::esx::dvs_add_hosts {'my-special-vmware-host.fqdn.tld':
-#     esx_host      => 'my_esx_hostname_or_ip_here',
-#     dvswitch_name => 'my_dvswitch_name_here',
-#     dvswitch_nic_defaults => {
-#     'my_dvswitch_name_here' => {
-#       'type' => distributed,
-#       'nics' => [ 'vmnic0', 'vmnic1']},
-#     'my_svswitch_name_here' => {
-#       'type' => standard,
-#       'nics' => [ 'vmnic2', 'vmnic3']}
-#     }
-#     overrides => {
-#     'my_dvswitch_name_here' => {
-#       'type' => distributed,
-#       'nics' => [ 'vmnic2', 'vmnic3']},
-#     'my_svswitch_name_here' => {
-#       'type' => standard,
-#       'nics' => [ 'vmnic0', 'vmnic1']}
-#     }
+#     nics          => [ 'vmnic2', 'vmnic3'],
 #   }
 define powercli::esx::dvs_add_hosts (
   $esx_host,
@@ -66,13 +24,11 @@ define powercli::esx::dvs_add_hosts (
 
   # For each phyysical nic to be added to the dvswitch..
   $nics.each | $nic | {
-    # Add the current $nic
+    # Add the current $nic to the $dvswitch_name if it is not already attached
     exec { "${name}: add ${nic} to ${dvswitch_name}":
       command  => template('powercli/powercli_esx_dvs_add_hosts.ps1.erb'),
       provider => 'powershell',
       onlyif   => template('powercli/powercli_esx_dvs_add_hosts_onlyif.ps1.erb'),
     }
-    $onlyif = template('powercli/powercli_esx_dvs_add_hosts_onlyif.ps1.erb')
-    #notify{ "${esx_host} - ${nic} onlyif is: ${onlyif}" : }
   }
 }
